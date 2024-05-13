2f4f97330d38fa5622964935eb4426dbd8b5dff9
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-05-13 23:11:15 +0900

    feat: microcmsの型定義と取得用の関数記述

---
+++ lib/microcms.ts
@@ -0,0 +1,50 @@
+import { createClient } from "microcms-js-sdk";
+import type { MicroCMSQueries } from "microcms-js-sdk";
+// ブログの型定義
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    name: string;
+  };
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+// API取得用のクライアントを作成
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+// ブログ一覧の取得
+export const getList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Blog>({
+    endpoint: "blogs",
+    queries,
+  })
+  return listData;
+}
+
+// ブログ詳細の取得
+export const getDetail = async (contentId: string, queries?: MicroCMSQueries) => {
+  const detailData = await client.getListDetail<Blog>({
+    endpoint: "blogs",
+    contentId,
+    queries
+  })
+  return detailData;
+}
